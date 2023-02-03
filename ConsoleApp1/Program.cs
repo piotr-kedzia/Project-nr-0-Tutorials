ddd29748e762113cbd4c34d5f0ea41273d51f4bf
Piotr Kedzia <piotrkedzia93@gmail.com>
2023-02-03 13:30:26 +0100

    New file: Program.cs

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Hello();
+
+
+        }
+
+        static void Hello()
+        {
+            List<string> names = new List<string> { "Ana", "Maria", "Felipe" };
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            Console.WriteLine("---------------------");
+        }
+
+
+    }
+    
+}
